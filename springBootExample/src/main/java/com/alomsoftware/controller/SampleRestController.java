c086594964af877aa94275d49d4316dafc529812
mjachowdhury <study.computing2016@gmail.com>
2017-08-23 19:34:06 +0100

    Setting Spring Boot
    -Used @RestController tag
    -@GetMapping("/") this means for homepage

--- springBootExample/src/main/java/com/alomsoftware/controller/SampleRestController.java
+++ springBootExample/src/main/java/com/alomsoftware/controller/SampleRestController.java
@@ -1,5 +1,14 @@
 package com.alomsoftware.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SampleRestController {
+	
+	@GetMapping("/")
+	public String salam(){
+		return "As salamu Alykum";
+	}
 
 }
